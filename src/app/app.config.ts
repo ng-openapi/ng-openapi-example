22a4b52b3b6540cdbbfbe7a641967b73736b31bd
tareq <tareq@thejami.com>
2025-07-18 17:49:09 +0200

    Initial commit example for ng-openapi

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
+import {provideNgOpenapi} from './client/generated';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideZoneChangeDetection({eventCoalescing: true}),
+    provideRouter(routes),
+    provideHttpClient(),
+    provideNgOpenapi({
+      basePath: 'https://petstore3.swagger.io/api/v3',
+    }),
   ]
 };
---
+++ src/app/client/openapi.config.ts
@@ -0,0 +1,22 @@
+import { GeneratorConfig } from 'ng-openapi';
+
+const config: GeneratorConfig = {
+  input: './swagger.json',
+  output: './generated',
+  options: {
+    dateType: 'Date',
+    enumStyle: 'enum',
+    generateEnumBasedOnDescription: true,
+    customizeMethodName: (operationId) => {
+      const methodName = operationId.split('_').at(-1) ?? operationId;
+      const camelCasedMethodName = methodName
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, '');
+      return camelCasedMethodName;
+    },
+  },
+};
+
+export default config;

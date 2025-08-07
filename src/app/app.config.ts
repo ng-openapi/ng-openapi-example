f9c0ae5ed88a11270fb2bcd2a7a387311b30d32a
tareq <tareq@thejami.com>
2025-08-08 01:07:25 +0200

    feat: Added url client example

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {AuthInterceptor, ErrorInterceptor, LoggingInterceptor, WarningInterceptor} from './interceptors/interceptors';
 import {providePetStoreJsonClient} from './clients/json-pet-store-client/generated';
 import {providePetStoreYamlClient} from './clients/yaml-pet-store-client/generated';
+import {providePetStoreUrlClient} from './clients/url-pet-store-client/generated';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,6 +16,10 @@ export const appConfig: ApplicationConfig = {
     providePetStoreJsonClient({
       basePath: 'https://petstore3.swagger.io/api/v3',
       interceptors: [ErrorInterceptor, AuthInterceptor, LoggingInterceptor, WarningInterceptor]
+    }),
+    providePetStoreUrlClient({
+      basePath: 'https://petstore3.swagger.io/api/v3',
+      interceptors: [ErrorInterceptor, AuthInterceptor, LoggingInterceptor]
     }),
     providePetStoreYamlClient({
       basePath: 'https://petstore3.swagger.io/api/v3',
---
+++ src/app/clients/url-pet-store-client/openapi.config.ts
@@ -0,0 +1,26 @@
+import { GeneratorConfig } from 'ng-openapi';
+
+const config: GeneratorConfig = {
+  input: 'https://petstore3.swagger.io/api/v3/openapi.json',
+  output: './generated',
+  clientName: 'PetStoreUrl',
+  validateInput: (spec) => {
+    return spec.info.title === "Swagger Petstore - OpenAPI 3.0";
+  },
+  options: {
+    dateType: 'Date',
+    enumStyle: 'enum',
+    generateEnumBasedOnDescription: true,
+    customizeMethodName: (operationId) => {
+      const methodName = operationId.split('_').at(-1) ?? operationId;
+      const camelCasedMethodName = methodName
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        })
+        .replace(/\s+/g, '');
+      return camelCasedMethodName;
+    },
+  },
+};
+
+export default config;
